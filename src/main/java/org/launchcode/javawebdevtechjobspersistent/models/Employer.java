21ea830f8e55cd8516a11962ccef9bb6db05feb8
Katie Layne <klayne1129@gmail.com>
2020-01-27 19:59:10 -0600

    Part 2 employer finished

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,31 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+@Entity
 public class Employer extends AbstractEntity {
 
+    @ManyToOne
+    @NotBlank(message = "location is required")
+    @Size(min = 3, max = 50, message = "Location must be between 3 and 150 characters")
+    private String location;
 
+    public Employer(@NotBlank(message = "location is required") @Size(min = 3, max = 50, message = "Location must be between 3 and 150 characters") String location) {
+        this.location = location;
+    }
+
+    public Employer() {
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
